b121162ce814dea685597c5731a0234ed57f3415
l10nel <l10ne1>
2019-04-09 23:46:32 +0800

    新增HeadlinesActivity

---
+++ app/src/main/java/com/lionel/gonews/headlines/HeadlinesActivity.java
@@ -0,0 +1,15 @@
+package com.lionel.gonews.headlines;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lionel.gonews.R;
+
+public class HeadlinesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
