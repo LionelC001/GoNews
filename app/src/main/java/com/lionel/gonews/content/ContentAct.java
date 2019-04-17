7e429bdc0d2b626988703fa571643b8aeeca1492
l10nel <l10ne1>
2019-04-17 13:19:07 +0800

    新增ContentAct的layout

--- app/src/main/java/com/lionel/gonews/content/ContentAct.java
+++ app/src/main/java/com/lionel/gonews/content/ContentAct.java
@@ -1,36 +1,37 @@
 package com.lionel.gonews.content;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.lionel.gonews.R;
 import com.lionel.gonews.data.News;
 
 import static com.lionel.gonews.util.Constants.NEWS_CONTENT;
 
 public class ContentAct extends AppCompatActivity {
 
+    private News news;
+    private ViewDataBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.act_content);
 
-        getNewsContent();
+        initNewsContent();
+
+    }
+
+    private void initNewsContent() {
+        news = getIntent().getParcelableExtra(NEWS_CONTENT);
+        binding.setVariable()
     }
 
     private void getNewsContent() {
-//        News news = (News)getIntent().getSerializableExtra(NEWS_CONTENT);
-//        Log.d("<>", "getNewsContent: " + news.source.name);
-        News news = getIntent().getParcelableExtra(NEWS_CONTENT);
-        Log.d("<>", "source id: " + news.source.id
-                + "\n source name: " + news.source.name
-                + "\n author: " + news.author
-                + "\n title: " + news.title
-                + "\n description: " + news.description
-                + "\n url: " + news.url
-                + "\n urlToImage: " + news.urlToImage
-                + "\n publishedAt: " + news.publishedAt
-                + "\n content: " + news.content
-                + "\n");
+
     }
 }
