d33675190c11730912034cf052532626d5d4207e
l10nel <l10ne1>
2019-04-18 20:22:43 +0800

    解決Drawer Layout被status bar擋住的問題

--- app/src/main/java/com/lionel/gonews/search/SearchAct.java
+++ app/src/main/java/com/lionel/gonews/search/SearchAct.java
@@ -1,8 +1,13 @@
 package com.lionel.gonews.search;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.lionel.gonews.R;
 
@@ -12,22 +17,45 @@ import java.util.List;
 public class SearchAct extends AppCompatActivity implements SearchBox.ISearchBoxCallback {
 
     private SearchBox searchBox;
+    private View layoutResult;
+    private TextView txtResultCount;
+    private ImageButton btnFilter;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_search);
 
-        initViews();
+
+        initSearchBox();
+        initLayoutResult();
+        initDrawerLayout();
 
         testSearchBox();
     }
 
-    private void initViews() {
+    private void initDrawerLayout() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+    }
+
+
+    private void initSearchBox() {
         searchBox = findViewById(R.id.searchBox);
         searchBox.setCallback(this);
     }
 
+    private void initLayoutResult() {
+        layoutResult = findViewById(R.id.layoutResult);
+        txtResultCount = findViewById(R.id.txtResultCount);
+        btnFilter = findViewById(R.id.imbBtnFilter);
+        btnFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+    }
 
     private void testSearchBox() {
 
