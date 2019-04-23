d9e03cdb1a1c23fd96214bc9ec2fd3c1a7917668
l10nel <l10ne1>
2019-04-23 20:52:37 +0800

    新增popupWindow開啟時, 遮蓋背景效果

--- app/src/main/java/com/lionel/gonews/base/BasePopupWindow.java
+++ app/src/main/java/com/lionel/gonews/base/BasePopupWindow.java
@@ -1,17 +1,25 @@
 package com.lionel.gonews.base;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.PopupWindow;
 
 import com.lionel.gonews.R;
 
 public class BasePopupWindow extends PopupWindow {
 
+    private final Context context;
+
     public BasePopupWindow(Context context) {
+        this.context = context;
+
+        initLayoutParams();
+    }
+
+    private void initLayoutParams() {
         setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
         setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
         setBackgroundDrawable(context.getDrawable(R.drawable.frame_round_corner_white));
@@ -25,11 +33,11 @@ public class BasePopupWindow extends PopupWindow {
     }
 
     public void show(View anchor) {
-        getContentView().measure(makeDropDownMeasureSpec(getWidth()),
-                makeDropDownMeasureSpec(getHeight()));
-
+        getContentView().measure(makeDropDownMeasureSpec(getWidth()), makeDropDownMeasureSpec(getHeight()));
         int offsetX = -getContentView().getMeasuredWidth();
         showAsDropDown(anchor, offsetX, 20, Gravity.START);
+
+        dimBehind();
     }
 
     private int makeDropDownMeasureSpec(int measureSpec) {
@@ -41,5 +49,14 @@ public class BasePopupWindow extends PopupWindow {
             mode = View.MeasureSpec.EXACTLY;
         }
         return View.MeasureSpec.makeMeasureSpec(View.MeasureSpec.getSize(measureSpec), mode);
+    }
+
+    private void dimBehind() {
+        View container = getContentView().getRootView();
+        WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
+        p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        p.dimAmount = 0.5f;
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.updateViewLayout(container, p);
     }
 }
