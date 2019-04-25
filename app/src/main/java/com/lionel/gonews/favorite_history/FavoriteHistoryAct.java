cb09a324b3b67439ca2abe7e0883915df717cd92
l10nel <l10ne1>
2019-04-25 15:53:02 +0800

    新增FavoriteHistoryAct

---
+++ app/src/main/java/com/lionel/gonews/favorite_history/FavoriteHistoryAct.java
@@ -0,0 +1,50 @@
+package com.lionel.gonews.favorite_history;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lionel.gonews.R;
+import com.lionel.gonews.base.BaseDisplayNewsListView;
+import com.lionel.gonews.base.IDisplayNewsList;
+import com.lionel.gonews.data.News;
+
+import static com.lionel.gonews.util.Constants.TYPE_NEWS_DATA;
+
+public class FavoriteHistoryAct extends AppCompatActivity implements IDisplayNewsList.IDisplayNewsListCallback {
+
+    private String typeData;
+    private FavoriteHistoryViewModel viewModel;
+    private BaseDisplayNewsListView displayNewsListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_favorite_history);
+
+        typeData = getIntent().getStringExtra(TYPE_NEWS_DATA);
+        viewModel = ViewModelProviders.of(this).get(FavoriteHistoryViewModel.class);
+
+        initDisplayNews();
+    }
+
+    private void initDisplayNews() {
+        displayNewsListView = findViewById(R.id.newsListView);
+        displayNewsListView.setCallback(this);
+    }
+
+    @Override
+    public void onRefreshNews() {
+
+    }
+
+    @Override
+    public void onLoadMoreNews() {
+
+    }
+
+    @Override
+    public void onIntentToNewsContent(News news) {
+
+    }
+}
---
+++ app/src/main/java/com/lionel/gonews/favorite_history/FavoriteHistoryViewModel.java
@@ -0,0 +1,11 @@
+package com.lionel.gonews.favorite_history;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class FavoriteHistoryViewModel extends AndroidViewModel {
+    public FavoriteHistoryViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
