7e6d0d7c937bfd3addb626fc7d700010b1e4aca8
l10nel <l10ne1>
2019-04-26 15:40:34 +0800

    Headlines Act 新增drawer layout視圖

--- app/src/main/java/com/lionel/gonews/headlines/HeadlinesAct.java
+++ app/src/main/java/com/lionel/gonews/headlines/HeadlinesAct.java
@@ -2,17 +2,21 @@ package com.lionel.gonews.headlines;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.lionel.gonews.R;
+import com.lionel.gonews.favorite_history.FavoriteHistoryAct;
 import com.lionel.gonews.search.SearchAct;
 
 import java.util.ArrayList;
@@ -20,6 +24,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.lionel.gonews.util.Constants.CATEGORYS;
+import static com.lionel.gonews.util.Constants.TYPE_ACT;
+import static com.lionel.gonews.util.Constants.TYPE_FAVORITE;
+import static com.lionel.gonews.util.Constants.TYPE_HISTORY;
 
 public class HeadlinesAct extends AppCompatActivity {
 
@@ -38,6 +45,29 @@ public class HeadlinesAct extends AppCompatActivity {
 
     private void initDrawerLayout() {
         drawerLayout = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.menu_favorite:
+                        intentToActivity(TYPE_FAVORITE);
+                        break;
+                    case R.id.menu_history:
+                        intentToActivity(TYPE_HISTORY);
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    private void intentToActivity(String type) {
+        Intent intent = new Intent();
+        intent.setClass(this, FavoriteHistoryAct.class);
+        intent.putExtra(TYPE_ACT, type);
+        startActivity(intent);
     }
 
     private void initBtnMenu() {
