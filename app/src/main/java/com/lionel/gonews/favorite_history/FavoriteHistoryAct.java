6e51ee9f438d0171423725f242f6dce896765fd7
l10nel <l10ne1>
2019-04-26 17:27:05 +0800

    FavoriteHistoryAct 和 LocalDataSource 串接, 初步完成

--- app/src/main/java/com/lionel/gonews/favorite_history/FavoriteHistoryAct.java
+++ app/src/main/java/com/lionel/gonews/favorite_history/FavoriteHistoryAct.java
@@ -1,20 +1,30 @@
 package com.lionel.gonews.favorite_history;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.lionel.gonews.R;
-import com.lionel.gonews.base.BaseDisplayNewsListView;
 import com.lionel.gonews.base.IDisplayNewsList;
+import com.lionel.gonews.content.ContentAct;
 import com.lionel.gonews.data.News;
+
+import java.util.List;
+
+import static com.lionel.gonews.util.Constants.NEWS_CONTENT;
+import static com.lionel.gonews.util.Constants.TYPE_ACT;
+import static com.lionel.gonews.util.Constants.TYPE_FAVORITE;
+import static com.lionel.gonews.util.Constants.TYPE_HISTORY;
 
 
 public class FavoriteHistoryAct extends AppCompatActivity implements IDisplayNewsList.IDisplayNewsListCallback {
 
-    private String typeData;
     private FavoriteHistoryViewModel viewModel;
-    private BaseDisplayNewsListView displayNewsListView;
+    private IDisplayNewsList newsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +34,54 @@ public class FavoriteHistoryAct extends AppCompatActivity implements IDisplayNew
         viewModel = ViewModelProviders.of(this).get(FavoriteHistoryViewModel.class);
 
         initDisplayNews();
+        initObserve();
+        initContent();
     }
 
     private void initDisplayNews() {
-        displayNewsListView = findViewById(R.id.newsListView);
-        displayNewsListView.setCallback(this);
+        newsListView = findViewById(R.id.newsListView);
+        newsListView.setCallback(this);
+    }
+
+    private void initObserve() {
+        viewModel.getNewsData().observe(this, new Observer<List<News>>() {
+            @Override
+            public void onChanged(@Nullable List<News> news) {
+                Log.d("<>", "new: " + news.size());
+                newsListView.showNews(news);
+            }
+        });
+    }
+
+    private void initContent() {
+        String type = getIntent().getStringExtra(TYPE_ACT);
+        if (type.equals(TYPE_FAVORITE)) {
+            initFavoriteContent();
+        } else if (type.equals(TYPE_HISTORY)) {
+            initHistoryContent();
+        }
+    }
+
+    private void initFavoriteContent() {
+
+        viewModel.getFavoriteNews().observe(this, new Observer<List<News>>() {
+            @Override
+            public void onChanged(@Nullable List<News> news) {
+                Log.d("<>", "new: " + news.size());
+                newsListView.showNews(news);
+            }
+        });
+    }
+
+    private void initHistoryContent() {
+
+        viewModel.getHistoryNews().observe(this, new Observer<List<News>>() {
+            @Override
+            public void onChanged(@Nullable List<News> news) {
+                Log.d("<>", "new: " + news.size());
+                newsListView.showNews(news);
+            }
+        });
     }
 
     @Override
@@ -43,6 +96,10 @@ public class FavoriteHistoryAct extends AppCompatActivity implements IDisplayNew
 
     @Override
     public void onIntentToNewsContent(News news) {
-
+        Log.d("<>", news.title);
+        Intent intent = new Intent();
+        intent.setClass(this, ContentAct.class);
+        intent.putExtra(NEWS_CONTENT, news);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/lionel/gonews/favorite_history/FavoriteHistoryViewModel.java
+++ app/src/main/java/com/lionel/gonews/favorite_history/FavoriteHistoryViewModel.java
@@ -2,10 +2,40 @@ package com.lionel.gonews.favorite_history;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 
+import com.lionel.gonews.data.News;
+import com.lionel.gonews.data.local.LocalNewsSource;
+
+import java.util.List;
+
 public class FavoriteHistoryViewModel extends AndroidViewModel {
+    private final LocalNewsSource localNewsSource;
+    private LiveData<List<News>> newsData = new MutableLiveData<>();
+
     public FavoriteHistoryViewModel(@NonNull Application application) {
         super(application);
+
+        localNewsSource = new LocalNewsSource(application.getApplicationContext());
     }
+
+    public LiveData<List<News>> getFavoriteNews() {
+//        newsData = localNewsSource.getAllFavoriteNews();
+        return localNewsSource.getAllFavoriteNews();
+    }
+
+    public LiveData<List<News>> getHistoryNews() {
+        return localNewsSource.getAllHistoryNews();
+    }
+
+    public LiveData<List<News>> getNewsData() {
+        return newsData;
+    }
+
+    public void deleteAllHistoryNews() {
+        localNewsSource.deleteAllHistory();
+    }
+
 }
